78ccf07afe06aac8195bcda1c51c5c069dd259e9
Robert E. Griffith <36312888+bobjunga@users.noreply.github.com>
2020-03-10 09:49:09 -0400

    initial commit

---
+++ spec/bg-tree-view-toolbar-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BgTreeViewToolbarView from '../lib/bg-tree-view-toolbar-view';
+
+describe('BgTreeViewToolbarView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
